0ee84e78982f3ae58317fc288aa92cef1aea7f35
bsmithft <70830813+bsmithft@users.noreply.github.com>
2022-09-24 19:03:34 -0500

    php update

--- dependencies/RanchHand - Website/mail/contact.php
+++ dependencies/RanchHand - Website/mail/contact.php
@@ -1,20 +1,14 @@
 <?php
-if(empty($_POST['name']) || empty($_POST['subject']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
-  http_response_code(500);
-  exit();
+if(isset($_POST['submit'])){
+  $name = $_POST['name'];
+  $subject = $_POST['subject'];
+  $mailFrom = $_POST['mail'];
+  $message = $_POST['message'];
+  $mailTo = "contact@ebtrsolutions.com";
+  $headers = "From: ".$mailFrom;
+  $txt = "You have recieved an e-mail from ".name.".\n\n".$message;
+
+  mail($mailTo, $subject, $txt, $headers);
+  header("Location: index.php?mailsend");
 }
-
-$name = strip_tags(htmlspecialchars($_POST['name']));
-$email = strip_tags(htmlspecialchars($_POST['email']));
-$m_subject = strip_tags(htmlspecialchars($_POST['subject']));
-$message = strip_tags(htmlspecialchars($_POST['message']));
-
-$to = "info@example.com"; // Change this email to your //
-$subject = "$m_subject:  $name";
-$body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\n\nEmail: $email\n\nSubject: $m_subject\n\nMessage: $message";
-$header = "From: $email";
-$header .= "Reply-To: $email";	
-
-if(!mail($to, $subject, $body, $header))
-  http_response_code(500);
 ?>
